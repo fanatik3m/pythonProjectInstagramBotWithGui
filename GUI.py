a1874e5ebca69a96d48cd4385617fbf4443553fa
Станислав <stanislavskuratovich37@gmail.com>
2023-04-28 21:38:54 +0300

    DONE v1.0

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 import datetime
 import sys
+from pathlib import Path
 from typing import List
 
 import multiprocessing
@@ -7,7 +8,8 @@ import threading
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QLineEdit, QPushButton, QWidget, QVBoxLayout, QLabel, QCheckBox
+    QApplication, QMainWindow, QLineEdit, QPushButton, QWidget, QVBoxLayout, QLabel, QCheckBox,
+    QFileDialog
 )
 
 from main import InstagramBot, log_in_set_likes
@@ -57,7 +59,7 @@ class MainWindow(QMainWindow):
         self.password_input.setPlaceholderText('your password...')
         self.password_input.textChanged.connect(self.get_password)
 
-        self.agree_label = QLabel('I agree with using my date in logging https://www.instagram.com/')
+        self.agree_label = QLabel('I agree with using my date in logging https://www.instagram.com/\n')
 
         self.agree_checkbox = QCheckBox()
         self.agree_checkbox.stateChanged.connect(self.check_agree)
@@ -73,8 +75,17 @@ class MainWindow(QMainWindow):
         self.start_button = QPushButton('Start working')
         self.start_button.clicked.connect(self.start_working)
 
+        self.filename_edit = QLabel('\nYour path to read urls (optionally)')
+
+        self.browse_button = QPushButton('Browse urls')
+        self.browse_button.clicked.connect(self.browse_from_file)
+
+        self.is_multiprocessing_label = QLabel('Should use multiprocessing?')
+
         self.check_multiprocessing = QCheckBox()
         self.check_multiprocessing.stateChanged.connect(self.change_multiprocessing)
+
+        self.file_dialog = QFileDialog(self)
 
         self.layout = QVBoxLayout()
         self.widgets = (
@@ -82,10 +93,13 @@ class MainWindow(QMainWindow):
             self.password_input,
             self.agree_checkbox,
             self.agree_label,
+            self.is_multiprocessing_label,
             self.check_multiprocessing,
             self.post_urls_label,
             self.post_urls_input,
             self.clear_button,
+            self.filename_edit,
+            self.browse_button,
             self.start_button
         )
 
@@ -97,16 +111,17 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(self.container)
 
-    def get_username(self, text):
+    def get_username(self, text: str) -> None:
         self.username = text
 
-    def get_password(self, text):
+    def get_password(self, text: str) -> None:
         self.password = text
 
-    def check_agree(self, status):
+    def check_agree(self, status) -> None:
+        print(status)
         self.is_agree = bool(status)
 
-    def save_clear(self):
+    def save_clear(self) -> None:
         text = self.post_urls_input.text()
         if text == '':
             return
@@ -114,13 +129,28 @@ class MainWindow(QMainWindow):
         self.post_urls.append(text)
         self.post_urls_input.clear()
 
-    def change_multiprocessing(self, status):
+    def change_multiprocessing(self, status) -> None:
         self.is_multiprocessing = bool(status)
-        print(self.is_multiprocessing)
 
-    def start_working(self):
+    def start_working(self) -> None:
         _args = (self.post_urls, self.username, self.password, self.is_multiprocessing)
         threading.Thread(target=main_logic, args=_args, name='webdriver-thread', daemon=True).start()
+
+    def browse_from_file(self) -> None:
+        filename, ok = QFileDialog.getOpenFileName(
+            self,
+            "Select a File",
+            ".",
+            "Text files (*.txt)"
+        )
+        if filename:
+            path = Path(filename)
+            self.filename_edit.setText(str(path))
+
+            with open(path, 'r') as file:
+                lines = [i.strip() for i in file.readlines()]
+                for line in lines:
+                    self.post_urls.append(line)
 
 
 app = QApplication(sys.argv)
